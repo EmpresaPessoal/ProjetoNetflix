b9db79593bd8fff9fe8d3f5899889be1f3318b74
Arthur <nunesarthur768@gmail.com>
2025-09-10 11:00:12 -0300

    9 - Arrumando o JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,53 @@
-//array de objetos com as imagens e informações do banner
-const bannerItems = [
-    {
-        imagem:"../assets/Game1.jpg",
-        titulo:"Titulo do filme 1",
-        descricao: "Filme 1"
-    },
-    {
-        imagem:"../assets/Game2.jpg",
-        titulo: "Titulo do filme 2",
-        descricao: "Filme 2"
-    },
-    {
-        imagem:"../assets/Game3.jpg",
-        titulo:"Titulo do filme 3",
-        descricao: "Filme 3"
-    }
+//ARRAY DE OBJETOS COM AS IMAGENS E INFORMAÇÕES DO BANNER
+const bannerItems =[
+{
+    imagem:"./src/assets/Game1.jpg",
+    titulo: "TItulo do filme 1",
+    descricao: "Filme 1"
+},
+{
+    imagem:"./src/assets/Game2.jpg",
+    titulo: "TItulo do filme 2",
+    descricao: "Filme 2"
+},
+
+{
+    imagem:"./src/assets/Game3.jpg",
+    titulo: "TItulo do filme 3",
+    descricao: "Filme 3"
+}
+
 ];
-// Declarando as variaveis e elementos com DOM
-const tempo = 5000; //tempo em milissegundos
+
+//DECLARANDO AS VARIAVEIS E ELEMENTOS COM DOM(DOCUMENT OBJECT MODEL)
+let i= 0;// inicio da contagem das imagens
+const tempo = 5000; // tempo em milissegundos
 const elementoBanner = document.querySelector(".banner");
 const elementoTitulo = document.querySelector(".banner-titulo");
 const elementoDescricao = document.querySelector(".banner-descricao");
-let i = 0; //inicio da contagem das imagens
 
-//função para o slideshow
-function slideshow(){
-    //altera a img de fundo do banner
-    //`` - template strings ou strings literais (forma de concateinar)
-    elementoBanner.style.backgroundImage = `url(${bannerItems[i].imagem}/)`;
-    //altera o titulo do banner
-    elementoTitulo.textContent = bannerItems[i].titulo;
-    // altera a descricao do banner 
+
+// FUNÇÃO PARA O SLIDESHOW
+
+function slideShow(){
+    // ALTERA A IMAEM DE FUNDO DO BANNER
+    // ``- template strings ou string literais forma de concatenar
+    elementoBanner.style.backgroundImage = `url(${bannerItems[i].imagem})`;
+    // ALTERA O TITULO DO BANNER
+    elementoTitulo.textContent =bannerItems[i].titulo;
+     // ALTERA O DESCRIÇÃO DO BANNER
     elementoDescricao.textContent = bannerItems[i].descricao;
 
-    //incrementa o indice (i) e reinicia quando chegar no final do array
-    //se o i for maior que a quantidade de imagens volta a 0
+    // INCREMENTA O INDECE (i) e reinicia quando chegar no final do array
+
+    // se o i for maior que a quantidade de imagems volta para o inicio
     i++;
-    if (i > bannerItems.length){
-        i = 0;
+    if( i >= bannerItems.length){
+        i =0;
     }
-    //chama a função novamente após o tempo definido
-    setTimeout(slideShow,tempo);
+    // chama a função noamente após o tempo definido
+    setTimeout(slideShow, tempo);
+
 }
-//inicia o slideshow
+// inicia o slideShow da função
 slideShow()
